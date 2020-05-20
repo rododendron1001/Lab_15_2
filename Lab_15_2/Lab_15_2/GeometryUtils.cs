44d843db9964c40e6da1dc9cc30fa049f52bc779
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-05-21 00:03:04 +0300

    Lab_15_2

--- Lab_15_2/Lab_15_2/GeometryUtils.cs
+++ Lab_15_2/Lab_15_2/GeometryUtils.cs
@@ -7,9 +7,30 @@ namespace Lab_15_2
     public static class GeometryUtils
     {
 
-		
+		public static void Translate_X(this Shape shape, int dx)
+			
+		{
+			
+			foreach (Point p in shape.Points)
+			{
+				p.Translate_x(dx);
+				
+			}
+		}
+
+		public static void Translate_Y(this Shape shape, int dy)
+
+		{
+
+			foreach (Point p in shape.Points)
+			{
+				p.Translate_y(dy);
+
+			}
+		}
 
 		public static int Calculate_Perimeter(Shape shape)
+
         {
             int perimeter = 0;
 
